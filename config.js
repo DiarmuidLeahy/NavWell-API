d301377e1c209b491118902850f6cd510c41e1b4
Derri Leahy <derri@live.ie>
2017-06-12 17:33:08 +0100

    revert

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ var configuration =
 		//connectionString:'mongodb://beta-user:1q2w3e4r@ds027809.mongolab.com:27809/coes-beta',
 		connectionString:'mongodb://navwelldb2:NavW3lldb@ds015690.mlab.com:15690/navwell',
 		port:4242,
-		//urlClient:'http://www.coescomunicacion.com:7000/metrix/',
+		urlClient:'http://www.coescomunicacion.com:7000/metrix/',
 		enableCORS:true,	//Cross origin requests
 		loggly_host: 'logs-01.loggly.com',
 		loggly_path: '/inputs/9b369756-ca21-4ed6-90e5-59de4455243d/tag/http/',
